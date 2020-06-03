ee015d1b5a13b8c1f4be88134b6c1f24322c1d32
GoldenhippoInTw\Toast.Tsao <Toast.Tsao@goldenhippo.net>
2020-06-03 18:18:39 +0800

    First Commit

---
+++ NetCore3_WindowsServiceSample/Controllers/SampleController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace NetCore3_WindowsServiceSample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SampleController : ControllerBase
+    {
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new{msg="hello"});
+        }
+    }
+}
